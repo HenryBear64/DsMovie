e56118f923caf3103a7c136ff9dc4905e38439c4
Henrique <henrybear18@outlook.com>
2022-01-16 15:29:35 -0300

    Domain model, database seed

--- Backend/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ Backend/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -1,5 +1,19 @@
 package com.devsuperior.demo.config;
 
+import java.util.Arrays;
+
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
 public class SucurityConfig {
 
 	@Configuration
